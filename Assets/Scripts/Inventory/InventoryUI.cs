2fcaac402edc1c7ee4fe89c0ccd8127e838ee56f
uwatcha <alterego102u3v3@gmail.com>
2025-03-04 11:04:14 +0900

    Dive画面で使う、ボタン機能を排除したItemWordIconを作成し、InventoryUIはEditorからIconとButtonのどちらを出すか選べるようにした

--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -3,21 +3,29 @@ using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
-    [SerializeField] private GameObject itemWordButtonPrefab;
+    // HomeSceneではButton、DiveSceneではIconをアタッチする。
+    [SerializeField] private GameObject itemWordUIPrefab;
     [SerializeField] private VerticalLayoutGroup inventoryGroup;
+    private ItemWordInventory itemWordInv;
 
     void Start()
     {
+        itemWordInv = ItemWordInventory.Instance;
         LoadInventoryData();
     }
 
     public void LoadInventoryData()
     {
-        foreach (ItemEntry item in ItemWordInventory.Instance.Inventory)
+        foreach (ItemEntry item in itemWordInv.Inventory)
         {
-            GameObject instance = Instantiate(itemWordButtonPrefab, inventoryGroup.gameObject.transform);
-            ItemWordButton ItemWordButton = instance.GetComponent<ItemWordButton>();
-            ItemWordButton.Initialize(item);
+            GameObject instance = Instantiate(itemWordUIPrefab, inventoryGroup.gameObject.transform);
+            ItemWordIcon itemWordIcon = instance.GetComponent<ItemWordIcon>(); //Buttonとアイコンの親クラス作る
+            itemWordIcon.Initialize(item);
         }
+    }
+
+    public void AddItemWord(ItemWord itemWord)
+    {
+        itemWordInv.AddItemWord(itemWord);
     }
 }
