235e78d63bef3334509af6e435b3ea678709ea38
uwatcha <alterego102u3v3@gmail.com>
2025-03-04 15:05:46 +0900

    SearchWorld.csで、そのWorld内で表示するItemWordの一覧と、背景画像を持たないようにした. (どちらも各WorldのScene内で配置すべき)

--- Assets/Scripts/SearchWorld/SearchWorld.cs
+++ Assets/Scripts/SearchWorld/SearchWorld.cs
@@ -9,11 +9,6 @@ public class SearchWorld : ScriptableObject
 {
     [SerializeField] private string id;
     [SerializeField] private string worldName;
-    [SerializeField] private Sprite sprite;
-    // SearchWorldの各シーンでSearchPointオブジェクトを配置するから、SearchWorldがitemWordを一括管理する必要はないかも
-    [SerializeField] private List<ItemWord> itemWords;
     public string Id => id; //csvに記述された、省略された名前
     public string WorldName => worldName; //正式な日本語の名前
-    public Sprite Sprite => sprite;
-    public List<ItemWord> ItemWords => itemWords;
 }
