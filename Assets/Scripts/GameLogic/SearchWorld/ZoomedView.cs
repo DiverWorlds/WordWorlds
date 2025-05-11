a27690b2eb071448d1a609871b214023c53243e2
uwatcha <alterego102u3v3@gmail.com>
2025-05-11 16:50:23 +0900

    WIP: WorldSystemをよりよく作り直す

--- Assets/Scripts/GameLogic/SearchWorld/ZoomedView.cs
+++ Assets/Scripts/GameLogic/SearchWorld/ZoomedView.cs
@@ -3,6 +3,8 @@ using UnityEngine.InputSystem;
 
 public class ZoomedView : MonoBehaviour
 {
+    //TODO: 複数カメラの切り替えで見た目を変更するようにする
+    //TODO: WorldView移動機能
     [SerializeField] private InputActionReference cancelAction;
 
     private InputAction action;
