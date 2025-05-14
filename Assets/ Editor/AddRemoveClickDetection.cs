8a4b96a631c5d0497808842281c3d3d3e7084587
uwatcha <alterego102u3v3@gmail.com>
2025-05-14 17:54:16 +0900

    Editor拡張で，ワンボタンでColliderとCameraSwitchTriggerをアタッチ/デタッチできるようにした．

---
+++ Assets/ Editor/AddRemoveClickDetection.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEditor;
+using Unity.VisualScripting;
+
+public class AddRemoveClickDetection : MonoBehaviour
+{
+    [MenuItem("GameObject/Click Detection/Add")]
+    private static void AddComponents(MenuCommand command)
+    {
+        GameObject selectedObject = command.context as GameObject;
+
+        if (selectedObject != null)
+        {
+            if (selectedObject.GetComponent<BoxCollider>() == null)
+            {
+                BoxCollider boxCollider = selectedObject.AddComponent<BoxCollider>();
+                boxCollider.size = new(boxCollider.size.x, boxCollider.size.y, 0.05f);
+            }
+            else
+            {
+                Logger.Log(selectedObject.name + " は既に BoxCollider を持っています。");
+            }
+
+            if (selectedObject.GetComponent<CameraSwitchTrigger>() == null)
+            {
+                selectedObject.AddComponent<CameraSwitchTrigger>();
+            }
+            else
+            {
+                Logger.Log(selectedObject.name + " は既に CameraSwitchTrigger スクリプトを持っています。");
+            }
+        }
+    }
+
+    [MenuItem("GameObject/Click Detection/Remove")]
+    private static void RemoveComponents(MenuCommand command)
+    {
+        GameObject selectedObject = command.context as GameObject;
+
+        if (selectedObject != null)
+        {
+            var boxCollider = selectedObject.GetComponent<BoxCollider>();
+            if (boxCollider != null)
+            {
+                DestroyImmediate(boxCollider);
+            }
+            else
+            {
+                Logger.Log(selectedObject.name + " は BoxCollider を持っていません。");
+            }
+
+            var cameraSwitchTrigger = selectedObject.GetComponent<CameraSwitchTrigger>();
+            if (cameraSwitchTrigger != null)
+            {
+                DestroyImmediate(cameraSwitchTrigger);
+            }
+            else
+            {
+                Logger.Log(selectedObject.name + " は CameraSwitchTrigger を持っていません。");
+            }
+        }
+    }
+}
