81bcce4d893c4cd9b94d618f25a17112a16b2eb3
uwatcha <alterego102u3v3@gmail.com>
2025-05-15 11:16:11 +0900

    SearchWorld/Rooms/SearchRoom/EventTriggersで，その配下のオブジェクトにMeshColliderをアタッチするようにした． SearchWorldをlegacyに送ってしまったので，戻す．

---
+++ Assets/ Editor/AddRemoveTriggersCollider.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEditor;
+using Unity.VisualScripting;
+
+public class AddRemoveTriggersCollider : MonoBehaviour
+{
+    private const string EventTriggers = "EventTriggers";
+
+    [MenuItem("GameObject/Triggers Collider/Add")]
+    private static void AddComponents()
+    {
+        GameObject activeGameObject = Selection.activeGameObject;
+        if (activeGameObject == null) return;
+
+        foreach (Transform child in activeGameObject.transform)
+        {
+            if (child.GetComponent<Collider>() == null)
+            {
+                child.AddComponent<MeshCollider>();
+            }
+            else
+            {
+                Logger.Log(child.gameObject.name + " は既に Collider を持っています。");
+            }
+
+        }
+        Logger.Log("Adding Collider To All EventTriggers Finished.");
+    }
+
+    [MenuItem("GameObject/Triggers Collider/Remove")]
+    private static void RemoveComponents()
+    {
+        GameObject activeGameObject = Selection.activeGameObject;
+        if (activeGameObject == null) return;
+
+        foreach (Transform child in activeGameObject.transform)
+        {
+            Collider coll = child.GetComponent<Collider>();
+            if (coll != null)
+            {
+                DestroyImmediate(coll);
+            }
+            else
+            {
+                Logger.Log(child.gameObject.name + " は Collider を持っていません。");
+            }
+
+        }
+        Logger.Log("Removing Collider To All EventTriggers Finished.");
+    }
+
+    [MenuItem("GameObject/Triggers Collider/Add", true)]
+    [MenuItem("GameObject/Triggers Collider/Remove", true)]
+    private static bool AddRemoveComponentsValidation()
+    {
+        return Selection.activeGameObject.CompareTag(EventTriggers);
+    }
+
+}
