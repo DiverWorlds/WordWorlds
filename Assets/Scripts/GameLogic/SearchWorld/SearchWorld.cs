2249772e2e718c5fbfd15c5423979e0bfa8a89ba
uwatcha <alterego102u3v3@gmail.com>
2025-06-22 23:21:41 +0900

    worldAppearance -> worldPreviewに改名

--- Assets/Scripts/GameLogic/SearchWorld/SearchWorld.cs
+++ Assets/Scripts/GameLogic/SearchWorld/SearchWorld.cs
@@ -9,14 +9,13 @@ public class SearchWorld : ScriptableObject
 {
     [SerializeField] private string id;
     [SerializeField] private string worldName;
-    // TODO: worldPreviewに改名
-    [SerializeField] private GameObject worldAppearance;
+    [SerializeField] private GameObject worldPreview;
     /// <summary>
     /// csvに記述された、省略された名前。
     /// この値はScene名と完全に一致している必要があります。
     /// </summary>
     public string Id => id;
     public string WorldName => worldName; //正式な日本語の名前
-    public GameObject WorldAppearance => worldAppearance; //世界のミニチュアのオブジェクトデータ
+    public GameObject WorldPreview => worldPreview; //世界のミニチュアのオブジェクトデータ
 
 }
