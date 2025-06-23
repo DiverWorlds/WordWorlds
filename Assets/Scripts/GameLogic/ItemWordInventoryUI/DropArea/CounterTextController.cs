2f8864af90dadaf961f16aabd2f41cf98489713f
uwatcha <alterego102u3v3@gmail.com>
2025-06-23 22:51:34 +0900

    DropAreaを作成した．

--- Assets/Scripts/GameLogic/ItemWordInventoryUI/DropArea/CounterTextController.cs
+++ Assets/Scripts/GameLogic/ItemWordInventoryUI/DropArea/CounterTextController.cs
@@ -1,6 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 public class CounterTextController : MonoBehaviour
 {
-    
+    private const int MAX_VALUE = 2;
+    [SerializeField] private TextMeshProUGUI counterText;
+
+    public void SetCounter(int count)
+    {
+        counterText.text = $"{count} / {MAX_VALUE}";
+    }
 }
--- Assets/Scripts/GameLogic/ItemWordInventoryUI/DropArea/DropArea.cs
+++ Assets/Scripts/GameLogic/ItemWordInventoryUI/DropArea/DropArea.cs
@@ -1,6 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DropArea : MonoBehaviour
 {
-    
+    private const int MAX_VALUE = 2;
+    [SerializeField] private List<Transform> dropSlots;
+    [SerializeField] private CounterTextController counterTextController;
+    private DragUIElement[] placedItemWords = new DragUIElement[MAX_VALUE];
+    private Action onAreaFullFilled;
+    public DragUIElement[] PlacedItemWords => placedItemWords;
+    public event Action OnAreaFullFilled { add => onAreaFullFilled += value; remove => onAreaFullFilled -= value; }
+
+    public void HandleDrop(DragUIElement droppedObject)
+    {
+        // 先着順でdropSlotにdroppedObjectを配置し，placedItemWordsに記録する．
+        for (int i = 0; i < MAX_VALUE; i++)
+        {
+            if (placedItemWords[i] == null)
+            {
+                placedItemWords[i] = droppedObject;
+                droppedObject.transform.localPosition = dropSlots[i].localPosition;
+                break;
+            }
+        }
+
+        int droppedObjectsCount = placedItemWords.Where(w => w != null).Count();
+        counterTextController.SetCounter(droppedObjectsCount);
+
+        if (droppedObjectsCount == MAX_VALUE)
+        {
+            onAreaFullFilled.Invoke();
+        }
+    }
 }
